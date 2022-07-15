2fd791df3602cabd22070aa562418cae61272fa0
Dwi Nurhadi Afriyanto <dwi.nurhadi17@student.uns.ac.id>
2022-07-15 15:52:57 +0700

    add another class

--- a.php
+++ a.php
@@ -1,8 +1,19 @@
 <?php
-require 'simple_html_dom.php';
+include 'simple_html_dom.php';
 
-$html = file_get_html('https://www.plazavea.com.pe/supermercado/');
+$html = file_get_html('https://www.plazavea.com.pe/avena-quaker-tradicional-bolsa-900g/p');
 // Find all element which class=foo
-$ret = $html->find('.Showcase__brand');
-print_r($ret);
+// $ret = $html->find('.Showcase__brand');
+// print_r($ret);
+
+// foreach($html->find('img') as $element)
+//     echo $element->src . '<br>';
+
+
+foreach($html->find('a') as $element)
+    echo $element->href . '<br>';
+
+    
+$html->clear(); 
+unset($html);
 ?>
